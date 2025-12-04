3927eedd4c05f0c400eb16c1dc3f0be67fe4a042
To Ngoc Hieu <hieudeptrai09@gmail.com>
2025-12-05 00:59:27 +0700

    change the config

--- api/config/database.php
+++ api/config/database.php
@@ -2,7 +2,7 @@
 class Database
 {
     private $host = "localhost";
-    private $db_name = "blogproduct";
+    private $db_name = "spbl";
     private $username = "root";
     private $password = "";
     public $conn;
