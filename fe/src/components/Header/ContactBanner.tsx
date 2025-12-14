d5d8980f5a3b04053c521b89782a4820ba2962b1
To Ngoc Hieu <hieudeptrai09@gmail.com>
2025-12-14 22:24:56 +0700

    change the contact banner

--- fe/src/components/Header/ContactBanner.tsx
+++ fe/src/components/Header/ContactBanner.tsx
@@ -5,21 +5,19 @@ export default function ContactBanner() {
   return (
     <div className="bg-primary text-white py-2">
       <div className="container-custom flex items-center justify-between">
-        <div className="flex items-center gap-4 text-sm ml-auto">
+        <div className="flex items-center gap-4 text-base ml-auto">
           <span className="hidden md:inline">Liên hệ ngay:</span>
           <a
             href={`tel:${shopInfo.phoneUrl}`}
             className="flex items-center gap-1 hover:font-semibold"
           >
-            <Phone size={14} />
-            <span>{shopInfo.phone}</span>
+            <Phone size={18} />
           </a>
           <a
             href={shopInfo.facebook}
             className="flex items-center gap-1 hover:font-semibold"
           >
-            <Facebook size={14} />
-            <span>{shopInfo.facebook}</span>
+            <Facebook size={18} />
           </a>
         </div>
       </div>
